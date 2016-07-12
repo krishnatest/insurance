87448c2e12947040cfd1766398cf63ef02cf57be
kkumar92 <kkumar92@csc.com>
2016-07-12 16:30:18 +0530

    added

--- insuranceClaim/src/UserAuthentication.java
+++ insuranceClaim/src/UserAuthentication.java
@@ -67,8 +67,8 @@ public class UserAuthentication extends HttpServlet {
 		boolean result = true;
 
 		//AppWorks Logical Error Introduce
-		if (userName == "") {
-		//if (userName.equals("")) {
+		//if (userName == "") {
+		if (userName.equals("")) {
 			error += "Please enter User Name.<br/>";
 			result = false;
 		}  
